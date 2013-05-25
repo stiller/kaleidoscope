a88eed3d34cfad4c994ea645b0de8c94b744e041
Joachim <joachim@nolten.org>
2013-05-25 12:37:00 +0200

    AST definition done

--- kaleidoscope.cpp
+++ kaleidoscope.cpp
@@ -1,5 +1,11 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <map>
+#include <vector>
+
 enum Token {
-  toke_eof = 1,
+  tok_eof = 1,
 
   //commands
   tok_def = -2, tok_extern = -3,
@@ -15,7 +21,7 @@ static int gettok() {
   static int LastChar = ' ';
 
   //skip any whitespace
-  while (isspace(LasttChar))
+  while (isspace(LastChar))
     LastChar = getchar();
 
   if (isalpha(LastChar)) { //identifier: [a-zA-Z[a-zA-Z0-9]*
@@ -42,7 +48,7 @@ static int gettok() {
   if (LastChar == "#") {
     // Comment until end of line
     do LastChar = getchar();
-    while(LastChar != EOD && LastChar != '\n' && LastChar != '\r');
+    while(LastChar != EOF && LastChar != '\n' && LastChar != '\r');
 
     if (LastChar != EOF)
       return gettok();
@@ -57,3 +63,61 @@ static int gettok() {
   LastChar = getchar();
   return ThisChar;
 }
+
+/// ExprAST - Base class for all expression nodes.
+class ExprAST {
+  public:
+    virtual ~ExprAST() {}
+};
+
+/// NumberExprAST - Expression class for numeric literars like "1.0"
+class NumberExprAST : public ExprAST {
+  double Val;
+public:
+  NumberExprAST(double val) : Val(val) {}
+};
+
+/// VariableExprAST - Expression class for referencing a variable, like "a".
+class VariableExprAST : public ExprAST {
+  std::string Name;
+public:
+  VariableExprAST(const std::string &name) : Name(name) {}
+};
+
+/// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+  char Op;
+  ExprAST *LHS, *RHS;
+public:
+  BinaryExprAST(char op, ExprAST *lhs, ExprAST *rhs)
+      : Op(op), LHS(lhs), RHS(rhs) {}
+};
+
+/// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST {
+  std::string Callee;
+  std::vector<ExprAST*> Args;
+public:
+  CallExprAST(const std::string &callee, std::vector<ExprAST*> &args)
+    : Callee(callee), Args(args) {}
+};
+
+/// PrototypeAST - This class represents the "prototype" for a function,
+/// which captures its name, and its argument names (thus implicitly the number
+/// of arguments the fucntion takes).
+class PrototypeAST {
+  std::string Name;
+  std::vector<std::string> Args;
+public:
+  PrototypeAST(const std::string &name, const std::vector<std::string> &args)
+    : Name(name), Args(args) {}
+};
+
+/// FunctionAST - This class represents a function definition itself.
+class FunctionAST {
+  PrototypeAST *Proto;
+  ExprAST *Body;
+public:
+  FunctionAST(PrototypeAST *proto, ExprAST *body)
+    : Proto(proto), Body(body) {}
+};
